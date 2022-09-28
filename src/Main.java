dd34813dc05d1d500f6f0e7c029059bcc68cd1bc
Анна <annakiseliova7@gmail.com>
2022-09-28 18:20:04 +0300

    Циклы. Часть 1. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Циклы. Часть 1. Задание 1
-        System.out.println("Циклы. Часть 1. Задание 1");
-        for (int i = 0; i < 10; i++) {
+        //Циклы. Часть 1. Задание 2
+        System.out.println("Циклы. Часть 1. Задание 2");
+        for (int i = 10; i > 0; i--) {
             System.out.println(i);
         }
     }
