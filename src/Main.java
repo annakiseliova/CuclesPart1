523632f7bd863f6f39c9fa7461351a2cabda639c
annakiseliova <annakiseliova7@gmail.com>
2022-10-03 17:27:24 +0300

    Циклы. Часть 1. Домашнее задание 2. Задание 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Циклы. Часть 1. Домашнее задание 2. Задание 1
+        System.out.println("Циклы. Часть 1. Домашнее задание 2. Задание 1");
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println("Високосный год " + i);
+        }
+    }
+}
