95342ca5fc1b24b5cb514922b83b7fa4595ca617
annakiseliova <annakiseliova7@gmail.com>
2022-10-03 17:50:06 +0300

    Циклы. Часть 1. Домашнее задание 3. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // Циклы. Часть 1. Домашнее задание 3. Задание 1
-        System.out.println("Циклы. Часть 1. Домашнее задание 3. Задание 1");
+        // Циклы. Часть 1. Домашнее задание 3. Задание 2
+        System.out.println("Циклы. Часть 1. Домашнее задание 3. Задание 2");
         int salary = 29000;
         int total = 0;
         for (int i = 1; i < 12; i = i + 1) {
+            total = total + total/100;
             total = total + salary;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
